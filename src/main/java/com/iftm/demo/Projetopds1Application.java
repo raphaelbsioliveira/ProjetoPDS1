708ba36a23cf2cf3c64c754169a7e81d667dfc66
raphaelbsioliveira <raphaell.26@hotmail.com>
2020-12-16 00:22:05 -0300

    Project created RaphaelD

---
+++ src/main/java/com/iftm/demo/Projetopds1Application.java
@@ -0,0 +1,15 @@
+package com.iftm.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetopds1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetopds1Application.class, args);
+		
+	
+	}
+
+}
---
+++ src/test/java/com/iftm/demo/Projetopds1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iftm.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetopds1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
