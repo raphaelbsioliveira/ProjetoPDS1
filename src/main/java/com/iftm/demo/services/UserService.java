1b0828713319f0a296826b1672cf3ea3304d6411
raphaelbsioliveira <raphaell.26@hotmail.com>
2020-12-19 17:38:46 -0300

    UserDTO, findById, findAll a14

--- src/main/java/com/iftm/demo/services/UserService.java
+++ src/main/java/com/iftm/demo/services/UserService.java
@@ -4,10 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.iftm.demo.entities.User;
 import com.iftm.demo.repositories.UserRepository;
+import com.iftm.demo.services.exceptions.ResourceNotFoundException;
+import com.iftm.demo.services.exceptions.DatabaseException;
+
+import javax.persistence.EntityNotFoundException;
+
 
 @Service
 public class UserService {
@@ -27,13 +34,23 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage()); 
+		}
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
